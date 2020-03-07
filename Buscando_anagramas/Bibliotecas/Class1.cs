6cf99c724b6b3c9490f50c17299342d481fb7f42
cstomboli <47337542+cstomboli@users.noreply.github.com>
2020-03-07 14:56:50 -0300

    Add files via upload

---
+++ Buscando_anagramas/Bibliotecas/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecas
+{
+    public class Class1
+    {
+        private string a;
+        private string b;
+
+        public Class1(string a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int Solutions()
+        {
+            a = a.ToLower();
+            b = b.ToLower();
+            //if (a.Length >= b.Length)
+            //{
+                
+            /*for(int i=0; a.Length>=i; )
+                {
+
+                   */
+                    Console.WriteLine("la contiene{0}", a.Length);
+                    Console.WriteLine("la contiene{0}", b.Length);
+                    if (String.Compare(a, b, true) == 0) //Esto compara si es exactamente esa palabra!
+                    {
+                        Console.WriteLine("la contiene");
+                    }
+                    
+                    //break;
+                
+                
+                /*
+                Console.WriteLine("la contiene{0}", a.Length);
+                Console.WriteLine("la contiene{0}", b.Length);
+                for (int i = b.Length; a.Length >= i; i++)
+                {
+                    Console.WriteLine("la for{0}",a.Length);
+                    Console.WriteLine("la for{0}",i);
+                    if (String.Compare(a,b)==0)
+                    {
+                        Console.WriteLine("la contiene");
+                    }
+                    bool result = a.Equals(b);
+                    if (result)
+                    {
+                        Console.WriteLine("la contiene");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No la contiene");
+                    }
+                    // if(strcmp())
+                    
+                }*/
+
+           // }
+
+            /* Esto no me anda!!
+            while(b.Length<=0)
+            {
+                if (String.Compare(a, b) == 0)
+                {
+                    Console.WriteLine("la contiene");
+                }
+
+            }*/
+            
+            return 0;
+
+        }
+    }
+}
---
+++ Buscando_anagramas/Buscando_anagramas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscando_anagramas
+{
+    class Program
+    {
+        /* Buscando anagramas:        
+        Se provee una cadena de caracteres de largo N y una subcadena de largo M.Se desea saber cuántas veces aparece la 
+        subcadena o un anagrama de la misma dentro de la cadena principal.
+        Por ejemplo, si se tiene la cadena A y la subcadena B
+        A = “hola, que buena ola Laomir” 
+        B = “OAL”  
+        El resultado deberá ser 3, dado lo siguiente A = “hola, que buena ola Laomir” 
+        Escribir una función: Int solution(string A, string B)
+        que devuelva la cantidad de veces que aparece B en A, o un anagrama de B en A.
+        Asumir: 
+        No hay distinción entre mayúsculas y  minúsculas N > M.
+        */
+        static void Main(string[] args)
+        {
+            int solucion = Solution("hola, que buena ola Laomir", "OAL");
+
+            int Solution(string A, string B)
+            {
+                A = A.ToLower();
+                B = B.ToLower();
+                int contador = 0;
+                int acumulador = 0;
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    for (int j = 0; j < B.Length; j++)
+                    {
+                        if (A[i] == B[j])
+                        {
+                            contador++;
+                            if ((contador == B.Length))
+                            {
+                                contador = 0;
+                                acumulador++;
+                            }
+                            break;
+                        }
+                    }
+                }
+                return acumulador;
+            }
+            Console.WriteLine("La cantidad de veces que aparece B en A es: {0}. ", solucion);
+            Console.ReadKey();
+        } 
+    }
+}
